c7d8bf69a1c61456b16f09c0405b8e6ebcd44462
Lawrence Splaver <lsplaver@yahoo.com>
2021-09-13 14:43:39 -0400

    Add project files.

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PriceQuotation.Models;
+
+namespace PriceQuotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = 0;
+            ViewBag.TotalPrice = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountAmount = model.CalculateDiscountAmount();
+                ViewBag.TotalPrice = model.CalculateTotalPrice();
+            }
+            else
+            {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.TotalPrice = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ PriceQuotation/Models/PriceQuotationModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a subtotal amount.")]
+        [Range(0.01, 999999999999999.99, ErrorMessage = "Subtotal must be greater than 0")]
+        public decimal? SubTotal { get; set; }
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(0, 100, ErrorMessage = "Discount percent must be between 0 and 100")]
+        public int? DiscountPercent { get; set; }
+        public decimal? DiscountAmount { get; set; }
+        public decimal? CalculateDiscountAmount()
+        {
+            decimal? discountPercent = 0;
+            discountPercent = (decimal?)(DiscountPercent / 100.00);
+            DiscountAmount = SubTotal * discountPercent;
+            return DiscountAmount;
+        }
+        
+        public decimal? CalculateTotalPrice()
+        {
+            decimal? totalPrice = 0;
+            totalPrice = SubTotal - DiscountAmount;
+            return totalPrice;
+        }
+    }
+}
